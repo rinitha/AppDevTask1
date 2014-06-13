bb9f59c3029d89fe609f37e1288809e4b100780b
Rinitha <rinitha1401@gmail.com>
2014-06-13 22:46:23 +0530

    first commit

--- src/com/delta/task1/MainActivity.java
+++ src/com/delta/task1/MainActivity.java
@@ -42,23 +42,27 @@ public class MainActivity extends ActionBarActivity
 			}
 		});
 
-		pause.setOnClickListener(new View.OnClickListener() {
+		pause.setOnClickListener(new View.OnClickListener() 
+		{
 
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(View arg0) 
+			{
 				// TODO Auto-generated method stub
-				timeWhenPaused += timeAtTheMoment;
+				timeWhenPaused += timeAtTheMoment; //storing the time when paused so that the display can be continued when the timer is started again, after pausing it
 				handlerForTimer.removeCallbacks(Timer);
 			}
 		});
 
-		reset.setOnClickListener(new View.OnClickListener() {
+		reset.setOnClickListener(new View.OnClickListener() 
+		{
 
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(View arg0) 
+			{
 				// TODO Auto-generated method stud
-				displayList();
-				startTime = 0L;
+				displayList(); // to display the recorded time in list below the buttons
+				startTime = 0L; // Reinitializing all variables to zero as timer is being reset
 				updatedTime = 0L;
 				timeWhenPaused = 0L;
 				timeAtTheMoment = 0L;
@@ -66,52 +70,54 @@ public class MainActivity extends ActionBarActivity
 				sec = 0;
 				min = 0;
 				hour = 0;
-				handlerForTimer.removeCallbacks(Timer);
+				handlerForTimer.removeCallbacks(Timer); // so ensure normal functioning of app when reset is clicked after start (skipping the pause button)
 			}
 		});
 
 	}
 
-	private Runnable Timer = new Runnable() {
-		public void run() {
-			timeAtTheMoment = SystemClock.uptimeMillis() - startTime;
-			updatedTime = timeAtTheMoment + timeWhenPaused;
+	private Runnable Timer = new Runnable() 
+	{
+		public void run() 
+		{
+			timeAtTheMoment = SystemClock.uptimeMillis() - startTime; // difference gives the number of milliseconds recorded
+			updatedTime = timeAtTheMoment + timeWhenPaused; // in case the timer was paused in between, updates time appropriately
 
-			sec = updatedTime / 1000;
+			sec = updatedTime / 1000; // to obtain in seconds, minutes and hours
 			min = sec / 60;
 			sec = sec % 60;
 			hour = min / 60;
 
-			if (hour > -1 && hour < 10)
+			if (hour > -1 && hour < 10) // to display as "00:00:00" instead of "0:00:00"
 				newhour = "0" + hour;
 			else
 				newhour = "" + hour;
 
-			tvTime.setText("" + newhour + ":" + String.format("%02d", min)
-					+ ":" + String.format("%02d", sec));
+			tvTime.setText("" + newhour + ":" + String.format("%02d", min)+ ":" + String.format("%02d", sec));// display updated(recorded) time
 
-			handlerForTimer.postDelayed(this, 0);
+			handlerForTimer.postDelayed(this, 0);// calling the runnable from within the runnable
 		}
 
 	};
 
-	private void displayList() {
+	private void displayList() 
+	{
 		// TODO Auto-generated method stub
-		if (hour > -1 && hour < 10)
+		if (hour > -1 && hour < 10) // to display as "00:00:00" instead of "0:00:00"
 			newhour = "0" + hour;
 		else
 			newhour = "" + hour;
 
-		tv5.setText(tv4.getText());
+		tv5.setText(tv4.getText()); //to make each new entry to the top of the list and keep moving the previous entry (if any) down
 		tv4.setText(tv3.getText());
 		tv3.setText(tv2.getText());
 		tv2.setText(tv1.getText());
-		tv1.setText("" + newhour + ":" + String.format("%02d", min) + ":"
-				+ String.format("%02d", sec));
+		tv1.setText("" + newhour + ":" + String.format("%02d", min) + ":"+ String.format("%02d", sec));
 
 	}
 
-	private void initialise() {
+	private void initialise() //  // function to reference the xml elements in java 
+	{
 		// TODO Auto-generated method stub
 		tvTime = (TextView) findViewById(R.id.tvTime);
 		tv1 = (TextView) findViewById(R.id.tv1);
